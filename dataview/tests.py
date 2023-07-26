f20898421b2dd733438499270634d126e1fadd97
Jonathan Harrison <jharrison12@gmail.com>
2023-07-26 19:43:58 -0400

    Initial commit

---
+++ dataview/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'dataview/home.html')
+
+class
