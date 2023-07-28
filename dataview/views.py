7844c5e22926977f7934fe623d7c9e37dc20c1ab
Jonathan Harrison <jharrison12@gmail.com>
2023-07-27 20:50:34 -0400

    Caregiver page shows two caregiver ids now

--- dataview/views.py
+++ dataview/views.py
@@ -6,5 +6,5 @@ def home_page(request):
     return render(request=request,template_name='dataview/home.html')
 
 def caregiver(request):
-    caregivers = Caregiver.objects.first()
+    caregivers = Caregiver.objects.all()
     return render(request=request, template_name='dataview/caregiver.html', context={'mother_or_caregiver':caregivers})
