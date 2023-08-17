dafca8c12f131bcd27a996837d6cce70c05cd9f4
Jonathan Harrison <jharrison12@gmail.com>
2023-08-17 19:47:02 -0400

    fixing typo in settings

--- CharmSampleTracking/settings.py
+++ CharmSampleTracking/settings.py
@@ -13,7 +13,7 @@ import os
 
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
-    SECRET_KEY = os.environ['DJANGO_SECRECT_KEY']
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
     DEBUG=True
