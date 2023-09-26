0c98161ca1ae8aed59d31153276e8ace541f7b52
Jonathan Harrison <jharrison12@gmail.com>
2023-09-26 08:21:45 -0400

    Format blood spot template for collected processed data

--- functional_tests/test_mother_biospecimen_blood_spots.py
+++ functional_tests/test_mother_biospecimen_blood_spots.py
@@ -16,3 +16,18 @@ class MotherBioSpecimenBloodspotsTest(FunctionalTest):
 
         body = self.browser.find_element(By.TAG_NAME,'body').text
         self.assertIn("ID: 1111BS", body)
+        self.assertIn("Log Status: Completed", body)
+
+        #User sees processed information if there is processed data
+        self.assertIn(body,'ID: 1111BS')
+        self.assertIn(body,'Collected Date Time:May 5, 2023, noon')
+        self.assertIn(body,'Quantity: 2')
+        self.assertIn(body,'Logged Date Time:May 5, 2023, 12:04 p.m.')
+        self.assertIn(body,'Processed Date Time:May 5, 2023, 12:04 p.m.')
+        ##TODO change
+        self.assertIn(body,'Logged By:BLANK')
+
+        #user goes to a respondent without processed ata and sees a form!
+
+        self.browser.get(self.live_server_url)
+        self.browser.get(f'{self.browser.current_url}biospecimen/caregiver/P7001/blood_spots/')
