83a6465221bdcc4127fdbd04c091293d67c5f540
Jonathan Harrison <jharrison12@gmail.com>
2023-11-06 08:41:00 -0500

    Remove extra test file.  Add test to check for report names

--- reports/tests/test_views.py
+++ reports/tests/test_views.py
@@ -6,5 +6,9 @@ logging.basicConfig(level=logging.CRITICAL)
 class ReportsPageTest(DatabaseSetup):
 
     def test_home_page_returns_correct_html(self):
-        response = self.client.get('reports/')
+        response = self.client.get(f'/reports/')
         self.assertTemplateUsed(response, 'reports/home.html')
+
+    def test_home_page_contains_caregiver_reports(self):
+        response = self.client.get(f'/reports/')
+        self.assertContains(response,'Caregiver Report')
