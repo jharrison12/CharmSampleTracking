16f3a6e8ae386e3b324d2509f67ceea666d794c7
Jonathan Harrison <jharrison12@gmail.com>
2023-11-27 15:17:11 -0500

    Refactor test views perinatal so that tests pass when going through chain of custodyu

--- biospecimen/tests/test_views/test_views_perinatal.py
+++ biospecimen/tests/test_views/test_views_perinatal.py
@@ -100,27 +100,36 @@ class CaregiverEcho2BiospecimenPagePlacenta(DatabaseSetup):
 
     def test_echo2_bio_entry_Placenta_shows_user_who_is_logged_in_after_submitted(self):
         primary_key = self.return_caregiver_bio_pk('P7000', 'Placenta')
-        response = self.collected_send_form(primary_key)
+        self.initial_send_form(primary_key,'C')
+        self.collected_send_form(primary_key)
         response = self.client.get(f'/biospecimen/caregiver/P7000/{primary_key}/entry/')
         self.assertContains(response,'Logged By: testuser')
 
     def test_echo2_bio_page_shows_shipped_to_wsu_data_if_complete(self):
-        primary_key = self.return_caregiver_bio_pk('P7001', 'Placenta')
-        caregiver_bio = CaregiverBiospecimen.objects.get(pk=primary_key)
+        primary_key = self.return_caregiver_bio_pk('P7000', 'Placenta')
+        self.initial_send_form(primary_key,'C')
+        self.collected_send_form(primary_key)
+        self.shipped_choice_send_form(primary_key,'W')
+        self.shipped_to_wsu_send_form(primary_key)
         response = self.client.get(f'/biospecimen/caregiver/P7000/{primary_key}/entry/')
         self.assertContains(response, 'Courier: FedEx')
 
     def test_echo2_bio_page_shows_shipped_by_user(self):
-        primary_key = self.return_caregiver_bio_pk('P7001', 'Placenta')
-        caregiver_bio = CaregiverBiospecimen.objects.get(pk=primary_key)
+        primary_key = self.return_caregiver_bio_pk('P7000', 'Placenta')
+        self.initial_send_form(primary_key, 'C')
+        self.collected_send_form(primary_key)
+        self.shipped_choice_send_form(primary_key, 'W')
+        self.shipped_to_wsu_send_form(primary_key)
         response = self.client.get(f'/biospecimen/caregiver/P7000/{primary_key}/entry/')
         self.assertContains(response, 'Shipped By: testuser')
 
     def test_echo2_bio_page_shows_shipped_to_echo_data_if_complete(self):
-        primary_key = self.return_caregiver_bio_pk('P7001', 'Placenta', 'T')
-        caregiver_bio = CaregiverBiospecimen.objects.get(pk=primary_key)
-        status_item = Status.objects.get(caregiverbiospecimen=caregiver_bio)
-        response = self.client.get(f'/biospecimen/caregiver/P7001/{primary_key}/entry/')
+        primary_key = self.return_caregiver_bio_pk('P7000', 'Placenta')
+        self.initial_send_form(primary_key,'C')
+        self.collected_send_form(primary_key)
+        self.shipped_choice_send_form(primary_key,'W')
+        self.shipped_to_wsu_send_form(primary_key)
+        response = self.client.get(f'/biospecimen/caregiver/P7000/{primary_key}/entry/')
         # logging.debug(response.content.decode())
         # print html RESPONSE CONTENT DECODE
         self.assertContains(response, 'Shipped Date Time:')
