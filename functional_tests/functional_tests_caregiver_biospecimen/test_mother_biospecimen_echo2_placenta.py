f5bd8fa756c7059869217c9a620c480b3449336a
Jonathan Harrison <jharrison12@gmail.com>
2023-12-04 07:30:34 -0500

    Refactor placenta functional tests

--- functional_tests/functional_tests_caregiver_biospecimen/test_mother_biospecimen_echo2_placenta.py
+++ functional_tests/functional_tests_caregiver_biospecimen/test_mother_biospecimen_echo2_placenta.py
@@ -16,7 +16,7 @@ class MotherBioSpecimenEcho2EntryTestPlacenta(FunctionalTest):
                                                         project_fk=echo2)
         return caregiverbio.pk
 
-    def test_user_can_choose_status_of_placenta_information_chooses_collected_shipped_wsu(self):
+    def test_user_can_choose_status_of_placenta_information_chooses_collected_shipped_wsu_shipped_echo(self):
         # User visits the caregiver biospecimen page and sees placenta
         primary_key = self.return_caregiver_bio_pk('P7000', 'Placenta')
         self.browser.get(self.live_server_url)
@@ -105,8 +105,7 @@ class MotherBioSpecimenEcho2EntryTestPlacenta(FunctionalTest):
         body = self.browser.find_element(By.TAG_NAME,'body').text
         self.assertIn('Received at WSU Form',body)
 
-        #User sees received date time at
-        #user submits shipped to WSu form
+        #User sees received form
         received_date_time = self.browser.find_element(By.ID,"id_received_at_wsu_form-received_date_time")
         received_date_time.clear()
         received_date_time.send_keys('2023-09-27 12:52:26')
@@ -117,87 +116,13 @@ class MotherBioSpecimenEcho2EntryTestPlacenta(FunctionalTest):
         body = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertIn('Received at WSU Sept. 27, 2023, 12:52 p.m.', body)
 
-    def test_user_can_choose_status_of_placenta_information_chooses_collected_shipped_echo(self):
-        # User visits the caregiver biospecimen page and sees placenta
-        primary_key = self.return_caregiver_bio_pk('P7000', 'Placenta')
-        self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/caregiver/P7000/{primary_key}/initial/')
+        #User sees shipped to echo form
+        shipped_echo_date_time = self.browser.find_element(By.ID,"id_shipped_echo_form-shipped_date_time")
+        shipped_echo_date_time.clear()
+        shipped_echo_date_time.send_keys('2023-09-27 12:52:26')
 
-        #user sees initial form and submits collected
-        header_text = self.browser.find_elements(By.TAG_NAME, 'h1')
-        self.assertIn('Charm ID: P7000', [item.text for item in header_text])
-        body_text = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('Initial Form',body_text)
-
-        collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_form-collected_not_collected_no_consent'))
-        collected_not_collected.select_by_visible_text('Collected')
-        submit = self.browser.find_element(By.XPATH,'//*[@id="collected_information"]/form/input[2]')
+        submit = self.browser.find_element(By.XPATH, '//*[@id="shipped_echo_form_information_form"]/form/input[2]')
         submit.click()
-
-        #user sees collected form on next page
-
-        form = self.browser.find_element(By.TAG_NAME,'form').text
-        self.assertIn('Collected Form',form)
-
-        #user submits form and sees data
-        collected = self.browser.find_element(By.ID,"id_placenta_form-collected_date_time")
-        collected.clear()
-        collected.send_keys('2023-09-27 12:52:26')
-
-        processed = self.browser.find_element(By.ID,"id_placenta_form-processed_date_time")
-        processed.send_keys('2023-09-27 12:52:26')
-
-        placed_in_formalin = self.browser.find_element(By.ID,"id_placenta_form-placed_in_formalin")
-        placed_in_formalin.send_keys('2023-09-27 12:52:26')
-
-        submit = self.browser.find_element(By.XPATH,'//*[@id="collected_information_form"]/form/input[2]')
-        submit.click()
-
-        body = self.browser.find_element(By.TAG_NAME,'body').text
-
-
-        #User sees incentive form
-
-        incentive_date = self.browser.find_element(By.ID,'id_incentive_form-incentive_date')
-        incentive_date.clear()
-        incentive_date.send_keys('2023-09-27')
-
-        submit = self.browser.find_element(By.XPATH,'//*[@id="incentive_information_form"]/form/input[2]')
-        submit.click()
-
-        body = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('Incentive Date: Sept. 27, 2023', body)
-
-
-        #user sees option to choose shipped to wsu or shipped to echo
-
-        form = self.browser.find_element(By.TAG_NAME,'form').text
-        self.assertIn('Shipped Choice Form',form)
-
-        shipped_to_echo = Select(self.browser.find_element(By.ID,'id_shipped_choice_form-shipped_to_wsu_or_echo'))
-        shipped_to_echo.select_by_visible_text('Shipped to Echo')
-
-        submit = self.browser.find_element(By.XPATH,'//*[@id="shipped_choice"]/form/input[2]')
-
-        submit.click()
-
-
-        body = self.browser.find_element(By.TAG_NAME,'body').text
-        needed_div = self.browser.find_element(By.ID,'shipped_to_echo_form').text
-        self.assertIn('Shipped to ECHO Form',needed_div)
-
-
-        shipped_to_echo_form = self.browser.find_element(By.ID,'id_shipped_to_echo_form-shipped_date_and_time')
-        shipped_to_echo_form.clear()
-        shipped_to_echo_form.send_keys('2023-09-27 12:52:26')
-
-        submit = self.browser.find_element(By.XPATH,'//*[@id="shipped_to_echo_form"]/form/input[2]')
-
-        submit.click()
-
-        body = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('Shipped Date Time: Sept. 27, 2023, 12:52 p.m.', body)
-
 
 
     def test_user_can_choose_status_of_placenta_information_chooses_not_collected(self):
@@ -212,7 +137,7 @@ class MotherBioSpecimenEcho2EntryTestPlacenta(FunctionalTest):
         body_text = self.browser.find_element(By.TAG_NAME,'body').text
         self.assertIn('Initial Form',body_text)
 
-        collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_form-collected_not_collected'))
+        collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_form-collected_not_collected_no_consent'))
         collected_not_collected.select_by_visible_text('Not Collected')
         submit = self.browser.find_element(By.XPATH,'//*[@id="collected_information"]/form/input[2]')
         submit.click()
