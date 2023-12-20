31171f2f31ac1b79d2be61b23436f6d444cafa3c
Jonathan Harrison <jharrison12@gmail.com>
2023-12-20 18:22:10 -0500

    Update blood spot ft to pass for send to wsu for zf

--- functional_tests/functional_tests_child_biospecimen/test_child_biospecimen_echo2_blood_spot.py
+++ functional_tests/functional_tests_child_biospecimen/test_child_biospecimen_echo2_blood_spot.py
@@ -11,7 +11,7 @@ class ChildBioSpecimenEntryBloodSpotZerotoFiveMonths(FunctionalTest):
     def return_child_bio_pk(self,child_id,collection_type,age):
         child_object = Child.objects.get(charm_project_identifier=child_id)
         child_biospecimen = ChildBiospecimen.objects.get(child_fk=child_object,
-                                                         collection_fk__collection_type_fk__collection_type=collection_type,
+                                                         collection_fk__collection_type=collection_type,
                                                          age_category_fk__age_category=age)
 
         return child_biospecimen.pk
@@ -22,13 +22,13 @@ class ChildBioSpecimenEntryBloodSpotZerotoFiveMonths(FunctionalTest):
 
     def test_user_can_choose_status_of_child_blood_spots_0_to_5_months_information_chooses_kit_sent_shipped_wsu(self):
         # User visits the caregiver biospecimen page and sees blood_spots
-        primary_key = self.return_child_bio_pk('7002M1', 'Bloodspots', 'ZF')
+        primary_key = self.return_child_bio_pk('4100F1', 'S', 'ZF')
         self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/7002M1/{primary_key}/initial/')
+        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
 
         # user sees initial form and submits collected
         body_text = self.browser.find_element(By.TAG_NAME, 'body').text
-        self.assertIn('ID: 7002M1', body_text)
+        self.assertIn('ID: 4100F1', body_text)
         self.assertIn('Initial Form', body_text)
 
         collected_not_collected = Select(
@@ -92,14 +92,6 @@ class ChildBioSpecimenEntryBloodSpotZerotoFiveMonths(FunctionalTest):
         text = self.webpage_text()
         self.assertIn('Sept. 30, 2023', text)
 
-        shipped_choice_form = self.browser.find_element(By.TAG_NAME, 'form').text
-        self.assertIn('Shipped to WSU', shipped_choice_form)
-
-        shipped_choice = Select(self.browser.find_element(By.ID, 'id_child_shipped_choice_form-shipped_to_wsu_or_echo'))
-        shipped_choice.select_by_visible_text('Shipped to WSU')
-        submit = self.browser.find_element(By.XPATH, '//*[@id="shipped_choice_form_div"]/form/input[2]')
-        submit.click()
-
         # User sees shipped to echo form and submits a date time
         wsu_shipped_form = self.browser.find_element(By.TAG_NAME, 'form').text
         self.assertIn('Shipped to WSU', wsu_shipped_form)
@@ -136,16 +128,30 @@ class ChildBioSpecimenEntryBloodSpotZerotoFiveMonths(FunctionalTest):
         body_text = self.webpage_text()
         self.assertIn('Received at WSU Date: Oct. 15, 2023, 12:52 p.m.',body_text)
 
+        #User sees shipped to echo form and submits a date time
+        echo_shipped_form = self.browser.find_element(By.TAG_NAME,'form').text
+        self.assertIn('Shipped to Echo',echo_shipped_form)
 
-    def test_user_can_choose_status_of_blood_spots_0_5_months_information_chooses_kit_sent_collected_shipped_echo(self):
+        echo_shipped_date = self.browser.find_element(By.ID, 'id_child_shipped_to_echo_form-shipped_date_and_time')
+        echo_shipped_date.clear()
+        echo_shipped_date.send_keys('2023-09-27 12:52:26')
+
+        submit = self.browser.find_element(By.XPATH,'//*[@id="shipped_to_echo_div"]/form/input[2]')
+        submit.click()
+
+        body_text = self.webpage_text()
+        self.assertIn('Shipped to Echo Date: Sept. 27, 2023',body_text)
+
+
+    def test_user_can_choose_status_of_blood_spots_twelve_to_thirteen_months_information_chooses_kit_sent_collected_shipped_echo(self):
         # User visits the caregiver biospecimen page and sees blood_spots
-        primary_key = self.return_child_bio_pk('7002M1', 'Bloodspots', 'ZF')
+        primary_key = self.return_child_bio_pk('4100F1', 'S', 'TT')
         self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/7002M1/{primary_key}/initial/')
+        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
 
         #user sees initial form and submits collected
         body_text = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('ID: 7002M1', body_text)
+        self.assertIn('ID: 4100F1', body_text)
         self.assertIn('Initial Form',body_text)
 
         collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_bio_form-collected_not_collected_kit_sent'))
@@ -236,12 +242,12 @@ class ChildBioSpecimenEntryBloodSpotZerotoFiveMonths(FunctionalTest):
 
     def test_user_can_choose_status_of_blood_spots_0_5_months_information_chooses_not_collected(self):
         # User visits the caregiver biospecimen page and sees blood_spots
-        primary_key = self.return_child_bio_pk('7002M1', 'Bloodspots', 'ZF')
+        primary_key = self.return_child_bio_pk('4100F1', 'S', 'ZF')
         self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/7002M1/{primary_key}/initial/')
+        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
         #user sees initial form and submits collected
         body_text = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('ID: 7002M1', body_text)
+        self.assertIn('ID: 4100F1', body_text)
         self.assertIn('Initial Form',body_text)
 
         collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_bio_form-collected_not_collected_kit_sent'))
@@ -259,12 +265,12 @@ class ChildBioSpecimenEntryBloodSpotZerotoFiveMonths(FunctionalTest):
 
     def test_user_can_choose_status_of_blood_spots_0_5_months_information_chooses_no_consent(self):
         # User visits the caregiver biospecimen page and sees blood_spots
-        primary_key = self.return_child_bio_pk('7002M1', 'Bloodspots', 'ZF')
+        primary_key = self.return_child_bio_pk('4100F1', 'S', 'ZF')
         self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/7002M1/{primary_key}/initial/')
+        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
         #user sees initial form and submits collected
         body_text = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('ID: 7002M1', body_text)
+        self.assertIn('ID: 4100F1', body_text)
         self.assertIn('Initial Form',body_text)
 
         collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_bio_form-collected_not_collected_kit_sent'))
@@ -295,13 +301,13 @@ class ChildBioSpecimenEntryBloodSpotTwelvetoTwentyThreeMonths(FunctionalTest):
 
 
     def test_user_can_choose_status_of_blood_spots_twelve_to_thirteen_months_no_consent(self):
-        primary_key = self.return_child_bio_pk('7002M1', 'Bloodspots', 'TT')
+        primary_key = self.return_child_bio_pk('4100F1', 'S', 'TT')
         self.browser.get(self.live_server_url)
         self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/7002M1/{primary_key}/initial/')
+        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
         # user sees initial form and submits collected
         body_text = self.browser.find_element(By.TAG_NAME, 'body').text
-        self.assertIn('ID: 7002M1', body_text)
+        self.assertIn('ID: 4100F1', body_text)
         self.assertIn('Initial Form', body_text)
 
         collected_not_collected = Select(
@@ -319,12 +325,12 @@ class ChildBioSpecimenEntryBloodSpotTwelvetoTwentyThreeMonths(FunctionalTest):
 
     def test_user_can_choose_status_of_blood_spots_twelve_to_thirteen_months_information_chooses_not_collected(self):
         # User visits the caregiver biospecimen page and sees blood_spots
-        primary_key = self.return_child_bio_pk('7002M1', 'Bloodspots', 'TT')
+        primary_key = self.return_child_bio_pk('4100F1', 'S', 'TT')
         self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/7002M1/{primary_key}/initial/')
+        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
         #user sees initial form and submits collected
         body_text = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('ID: 7002M1', body_text)
+        self.assertIn('ID: 4100F1', body_text)
         self.assertIn('Initial Form',body_text)
 
         collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_bio_form-collected_not_collected_kit_sent'))
@@ -341,13 +347,13 @@ class ChildBioSpecimenEntryBloodSpotTwelvetoTwentyThreeMonths(FunctionalTest):
 
     def test_user_can_choose_status_of_blood_spots_twelve_to_thirteen_months_chooses_kit_sent_collected_shipped_echo(self):
         # User visits the caregiver biospecimen page and sees blood_spots
-        primary_key = self.return_child_bio_pk('7002M1', 'Bloodspots', 'TT')
+        primary_key = self.return_child_bio_pk('4100F1', 'S', 'TT')
         self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/7002M1/{primary_key}/initial/')
+        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
 
         # user sees initial form and submits collected
         body_text = self.browser.find_element(By.TAG_NAME, 'body').text
-        self.assertIn('ID: 7002M1', body_text)
+        self.assertIn('ID: 4100F1', body_text)
         self.assertIn('Initial Form', body_text)
 
         collected_not_collected = Select(
