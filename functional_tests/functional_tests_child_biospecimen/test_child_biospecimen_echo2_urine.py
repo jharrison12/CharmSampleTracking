6f28874fe02a5e8f5253e7a222023b67bec7067a
Jonathan Harrison <jharrison12@gmail.com>
2023-12-20 18:07:42 -0500

    update child bio echo2 urine ft to pass

--- functional_tests/functional_tests_child_biospecimen/test_child_biospecimen_echo2_urine.py
+++ functional_tests/functional_tests_child_biospecimen/test_child_biospecimen_echo2_urine.py
@@ -93,17 +93,7 @@ class ChildBioSpecimenEntryUrine(FunctionalTest):
         text = self.webpage_text()
         self.assertIn('Sept. 30, 2023', text)
 
-        shipped_choice_form = self.browser.find_element(By.TAG_NAME, 'form').text
-        self.assertIn('Shipped to WSU', shipped_choice_form)
-
-        shipped_choice = Select(self.browser.find_element(By.ID, 'id_child_shipped_choice_form-shipped_to_wsu_or_echo'))
-        shipped_choice.select_by_visible_text('Shipped to WSU')
-        submit = self.browser.find_element(By.XPATH, '//*[@id="shipped_choice_form_div"]/form/input[2]')
-        submit.click()
-
-        # User sees shipped to echo form and submits a date time
-        wsu_shipped_form = self.browser.find_element(By.TAG_NAME, 'form').text
-        self.assertIn('Shipped to WSU', wsu_shipped_form)
+        #user sees shipped to wsu form
 
         shipped_date_time = self.browser.find_element(By.ID, 'id_child_shipped_to_wsu_form-shipped_date_and_time')
         shipped_date_time.clear()
@@ -129,86 +119,6 @@ class ChildBioSpecimenEntryUrine(FunctionalTest):
         text = self.webpage_text()
         self.assertIn('Received at WSU Date: Sept. 30, 2023, 12:52',text)
 
-
-    def test_user_can_choose_status_of_urine_information_chooses_kit_sent_collected_shipped_echo(self):
-        # User visits the caregiver biospecimen page and sees urine
-        primary_key = self.return_child_bio_pk('4100F1', 'U', 'ZF')
-        self.browser.get(self.live_server_url)
-        self.browser.get(f'{self.browser.current_url}biospecimen/child/4100F1/{primary_key}/initial/')
-
-        #user sees initial form and submits collected
-        body_text = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertIn('ID: 4100F1', body_text)
-        self.assertIn('Initial Form',body_text)
-
-        collected_not_collected = Select(self.browser.find_element(By.ID,'id_initial_bio_form-collected_not_collected_kit_sent'))
-        collected_not_collected.select_by_visible_text('Kit Sent')
-        submit = self.browser.find_element(By.XPATH,'//*[@id="collected_information"]/form/input[2]')
-        submit.click()
-
-        #user sees collected form on next page
-
-        body_text = self.browser.find_element(By.TAG_NAME,'body').text
-        self.assertNotIn('<form>', body_text)
-        self.assertIn('Kit sent date:', body_text)
-
-        kit_sent_date = self.browser.find_element(By.ID,"id_kit_sent_form-kit_sent_date")
-        kit_sent_date.clear()
-        kit_sent_date.send_keys('2023-09-27')
-
-        biospecimen_id = self.browser.find_element(By.ID,'id_kit_sent_form-echo_biospecimen_id')
-        biospecimen_id.send_keys('5555555')
-
-        submit = self.browser.find_element(By.XPATH,'//*[@id="initial_information"]/form/input[2]')
-        submit.click()
-
-        body_text = self.browser.find_element(By.TAG_NAME, 'body').text
-        self.assertIn('Sept. 27, 2023',body_text)
-
-        #user now sees the collected form
-
-        collected_form = self.browser.find_element(By.TAG_NAME,'form').text
-        self.assertIn('Collected',collected_form)
-        self.assertNotIn('Incentive date',collected_form)
-
-        in_person_remote = Select(self.browser.find_element(By.ID,'id_collected_child_form-in_person_remote'))
-        in_person_remote.select_by_visible_text('In Person')
-
-        date_received = self.browser.find_element(By.ID,'id_collected_child_form-date_received')
-        date_received.clear()
-        date_received.send_keys('2023-09-27')
-
-        number_of_tubes = self.browser.find_element(By.ID,'id_collected_child_form-number_of_tubes')
-        number_of_tubes.send_keys(5)
-
-        submit = self.browser.find_element(By.XPATH,'//*[@id="collected_information"]/form/input[2]')
-        submit.click()
-
-        #USer now sees the collected information and the shipped to echo ro shipped to wsu form
-
-        body_text = self.webpage_text()
-        self.assertIn('In Person or Remote: In Person',body_text)
-
-        incentive_form = self.browser.find_element(By.TAG_NAME,'form').text
-        self.assertIn('Incentive',incentive_form)
-
-        incentive_date = self.browser.find_element(By.ID,'id_child_incentive_form-incentive_date')
-        incentive_date.send_keys('2023-09-30')
-
-        submit = self.browser.find_element(By.XPATH,'//*[@id="incentive_form_div"]/form/input[2]')
-        submit.click()
-
-        text = self.webpage_text()
-        self.assertIn('Sept. 30, 2023',text)
-
-        shipped_choice_form = self.browser.find_element(By.TAG_NAME,'form').text
-        self.assertIn('Shipped to WSU',shipped_choice_form)
-
-        shipped_choice = Select(self.browser.find_element(By.ID,'id_child_shipped_choice_form-shipped_to_wsu_or_echo'))
-        shipped_choice.select_by_visible_text('Shipped to Echo')
-        submit = self.browser.find_element(By.XPATH,'//*[@id="shipped_choice_form_div"]/form/input[2]')
-        submit.click()
-
         #User sees shipped to echo form and submits a date time
         echo_shipped_form = self.browser.find_element(By.TAG_NAME,'form').text
         self.assertIn('Shipped to Echo',echo_shipped_form)
@@ -223,6 +133,7 @@ class ChildBioSpecimenEntryUrine(FunctionalTest):
 
         body_text = self.webpage_text()
         self.assertIn('Shipped to Echo Date: Sept. 27, 2023',body_text)
+
 
 
     def test_user_can_choose_status_of_urine_information_chooses_not_collected(self):
