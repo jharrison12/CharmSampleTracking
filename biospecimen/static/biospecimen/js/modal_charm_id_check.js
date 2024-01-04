d8735c4f0c6cb8644e3e4590638a5418b84aebfa
Jonathan Harrison <jharrison12@gmail.com>
2024-01-03 20:19:10 -0500

    Confirming id now links to biospecimen page.  I need to fix button

--- biospecimen/static/biospecimen/js/modal_charm_id_check.js
+++ biospecimen/static/biospecimen/js/modal_charm_id_check.js
@@ -10,11 +10,24 @@ if (exampleModal) {
 
     // Update the modal's content.
     const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
     const modalBody = exampleModal.querySelector('.modal-body')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+    const modalLink = exampleModal.querySelector('#modal-link')
+    console.log(modalLink)
 
     modalTitle.textContent = 'Confirm'
-    modalBody.textContent = `Please confirm charm id ${recipient}`
-    modalBodyInput.value = recipient
+    modalBody.textContent = `Please confirm charm id ${recipient}`;
+    //Tried to use Django links but apparently that is bad practice
+    //https://stackoverflow.com/questions/37311042/call-django-urls-inside-javascript-on-click-event
+    modalLink.setAttribute('href',`/biospecimen/charm_ids/${recipient}/`)
+    console.log(modalLink)
+
+
+
   })
 }
+
+    //
+    // const modalLink = exampleModal.getElementsByTagName('a')
+  // modalLink.setAttribute('href',`/biospecimen/charm_ids/${recipient}/`)
+  //modalLink.href = `{% url "biospecimen:list_of_bio_ids" ${recipient} %}`
