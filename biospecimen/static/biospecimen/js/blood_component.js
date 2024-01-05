e499bbfbfb6a91f6980249e2f723a7bbd56d1162
Jonathan Harrison <jharrison12@gmail.com>
2024-01-05 16:06:13 -0500

    Div ids for all blood components and also make serum appear when component clicked

--- biospecimen/static/biospecimen/js/blood_component.js
+++ biospecimen/static/biospecimen/js/blood_component.js
@@ -1,13 +1,16 @@
 //Hid component if not clicked
 $('#whole_blood_number_of_tubes_div').hide();
+$('#serum_number_of_tubes_div').hide();
 console.log('reached this')
 $('#id_blood_form-whole_blood').click(function() {
   $('#whole_blood_number_of_tubes_div')[this.checked ? "show" : "hide"]();
+});
+$('#id_blood_form-serum').click(function() {
+  $('#serum_number_of_tubes_div')[this.checked ? "show" : "hide"]();
 });
 
 // Reload javascript if user hits back button.
 $(document).ready(function(e) {
     var $input = $('#refresh');
-
     $input.val() == 'yes' ? location.reload(true) : $input.val('yes');
 });
