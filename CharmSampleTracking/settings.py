5f984241280cf5c18f5ebeb94f6d77c6eafef306
Jonathan Harrison <jharrison12@gmail.com>
2024-03-12 19:29:19 -0400

    Migrate from sqllite to mysql

--- CharmSampleTracking/settings.py
+++ CharmSampleTracking/settings.py
@@ -87,10 +87,22 @@ WSGI_APPLICATION = 'CharmSampleTracking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'charm_bio_track',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
