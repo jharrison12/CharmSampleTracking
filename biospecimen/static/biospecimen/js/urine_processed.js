7ea286e1b6726537370ca2dcc497cdeb9f01c3c4
Jonathan Harrison <jharrison12@gmail.com>
2024-04-17 17:40:18 -0400

    finalize kluge javascript for urine collection page

--- biospecimen/static/biospecimen/js/urine_processed.js
+++ biospecimen/static/biospecimen/js/urine_processed.js
@@ -1,4 +1,5 @@
 //Hide component if not clicked
+//TODO FIX ALL OF THIS
 $('#processed_aliquoted_date_time').hide();
 $('#refigerated_placed_date_time').hide();
 $('#refigerated_removed_date_time').hide();
@@ -16,6 +17,14 @@ $('#partial_aliquot_18ml_6').hide();
 $('#partial_aliquot_18ml_6_amount').hide();
 $('#partial_aliquot_18ml_7').hide();
 $('#partial_aliquot_18ml_7_amount').hide();
+$('#partial_aliquot_7ml_1').hide();
+$('#partial_aliquot_7ml_1_amount').hide();
+$('#partial_aliquot_7ml_2').hide();
+$('#partial_aliquot_7ml_2_amount').hide();
+$('#partial_aliquot_7ml_3').hide();
+$('#partial_aliquot_7ml_3_amount').hide();
+$('#partial_aliquot_7ml_4').hide();
+$('#partial_aliquot_7ml_4_amount').hide();
 
 
 console.log('reached this')
@@ -28,6 +37,11 @@ document.getElementById('id_processed_form-processed_aliquoted_off_site').addEve
 document.getElementById('id_processed_form-all_18_collected').addEventListener('change', function () {
     var style = this.value == 'N'  ? 'block' : 'none';
     document.getElementById('partial_aliquot_18ml_1').style.display = style;
+});
+
+document.getElementById('id_processed_form-all_7_collected').addEventListener('change', function () {
+    var style = this.value == 'N'  ? 'block' : 'none';
+    document.getElementById('partial_aliquot_7ml_1').style.display = style;
 });
 
 $('#id_processed_form-refigerated_prior_to_processing').click(function() {
@@ -63,6 +77,21 @@ $('#id_processed_form-partial_aliquot_18ml_7').click(function() {
   $('#partial_aliquot_18ml_7_amount')[this.checked ? "show" : "hide"]();
 });
 
+$('#id_processed_form-partial_aliquot_7ml_1').click(function() {
+  $('#partial_aliquot_7ml_1_amount')[this.checked ? "show" : "hide"]();
+});
+
+$('#id_processed_form-partial_aliquot_7ml_2').click(function() {
+  $('#partial_aliquot_7ml_2_amount')[this.checked ? "show" : "hide"]();
+});
+
+$('#id_processed_form-partial_aliquot_7ml_3').click(function() {
+  $('#partial_aliquot_7ml_3_amount')[this.checked ? "show" : "hide"]();
+});
+
+$('#id_processed_form-partial_aliquot_7ml_4').click(function() {
+  $('#partial_aliquot_7ml_4_amount')[this.checked ? "show" : "hide"]();
+});
 
 document.getElementById('id_processed_form-partial_aliquot_18ml_1_amount').addEventListener('change', function () {
     var style = this.value != '' ? 'block' : 'none';
@@ -94,6 +123,20 @@ document.getElementById('id_processed_form-partial_aliquot_18ml_6_amount').addEv
     document.getElementById('partial_aliquot_18ml_7').style.display = style;
 });
 
+document.getElementById('id_processed_form-partial_aliquot_7ml_1_amount').addEventListener('change', function () {
+    var style = this.value != '' ? 'block' : 'none';
+    document.getElementById('partial_aliquot_7ml_2').style.display = style;
+});
+
+document.getElementById('id_processed_form-partial_aliquot_7ml_2_amount').addEventListener('change', function () {
+    var style = this.value != '' ? 'block' : 'none';
+    document.getElementById('partial_aliquot_7ml_3').style.display = style;
+});
+
+document.getElementById('id_processed_form-partial_aliquot_7ml_3_amount').addEventListener('change', function () {
+    var style = this.value != '' ? 'block' : 'none';
+    document.getElementById('partial_aliquot_7ml_4').style.display = style;
+});
 
 // Reload javascript if user hits back button.
 $(document).ready(function(e) {
