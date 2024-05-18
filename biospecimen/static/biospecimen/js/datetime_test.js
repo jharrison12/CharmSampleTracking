3fb80623ffb297d6c7c7e1f90e1c59f6f9bc5588
Jonathan Harrison <jharrison12@gmail.com>
2024-05-18 19:54:33 -0400

    Change r to R in refrigerated.  Comment out lines in datetime_test to see if that fixes the error of the picker not popping up

--- biospecimen/static/biospecimen/js/datetime_test.js
+++ biospecimen/static/biospecimen/js/datetime_test.js
@@ -1,8 +1,8 @@
-$(".datetimepicker").flatpickr({
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
-});
-
-$(".datepicker").flatpickr({
-    dateFormat: "Y-m-d",
-});
+// $(".datetimepicker").flatpickr({
+//     enableTime: true,
+//     dateFormat: "Y-m-d H:i",
+// });
+//
+// $(".datepicker").flatpickr({
+//     dateFormat: "Y-m-d",
+// });
