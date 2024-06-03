18fc3e641900d9644fd61a2d6a33fef23a52ed9f
Jonathan Harrison <jharrison12@gmail.com>
2024-06-03 09:20:42 -0400

    Refactor portion of urine processed js.  I need to test

--- biospecimen/static/biospecimen/js/urine_processed.js
+++ biospecimen/static/biospecimen/js/urine_processed.js
@@ -11,26 +11,40 @@ $('#partial_aliquot_7ml_4_amount').hide();
 
 console.log('reached this')
 
-document.getElementById('id_processed_form-processed_aliquoted_off_site').addEventListener('change', function () {
-    var style = this.value == 'R' || this.value=='T' ? 'block' : 'none';
-    document.getElementById('processed_aliquoted_date_time').style.display = style;
-});
+window.addEventListener("DOMContentLoaded", (event) => {
 
-document.getElementById('id_processed_form-all_18_collected').addEventListener('change', function () {
-    var style = this.value == "False"  ? 'block' : 'none';
-    document.getElementById('partial_aliquot_18ml_volume').style.display = style;
-    document.getElementById('number_of_tubes_collected_18_ml_if_some_missing').style.display = style;
-});
+    const processed_aliquoted_off_site = document.getElementById('id_processed_form-processed_aliquoted_off_site')
+    if(processed_aliquoted_off_site){
+        processed_aliquoted_off_site.addEventListener('change', function () {
+        var style = this.value == 'R' || this.value == 'T' ? 'block' : 'none';
+        document.getElementById('processed_aliquoted_date_time').style.display = style;
+        });
+    }
 
-document.getElementById('id_processed_form-all_7_collected').addEventListener('change', function () {
-    var style = this.value == "False"  ? 'block' : 'none';
-    document.getElementById('partial_aliquot_7ml_volume').style.display = style;
-    document.getElementById('partial_aliquot_7ml_4_amount').style.display = style;
-});
+    const all_18_collected = document.getElementById('id_processed_form-all_18_collected')
+    if (all_18_collected){
+    all_18_collected.addEventListener('change', function () {
+        var style = this.value == "False" ? 'block' : 'none';
+        document.getElementById('partial_aliquot_18ml_volume').style.display = style;
+        document.getElementById('number_of_tubes_collected_18_ml_if_some_missing').style.display = style;
+        });
+    };
 
-$('#id_processed_form-refrigerated_prior_to_processing').click(function() {
-  $('#refrigerated_placed_date_time')[this.checked ? "show" : "hide"]();
-  $('#refrigerated_removed_date_time')[this.checked ? "show" : "hide"]();
+    const all_7_collected =document.getElementById('id_processed_form-all_7_collected')
+    if (all_7_collected){
+    all_7_collected.addEventListener('change', function () {
+        var style = this.value == "False" ? 'block' : 'none';
+        document.getElementById('partial_aliquot_7ml_volume').style.display = style;
+        document.getElementById('partial_aliquot_7ml_4_amount').style.display = style;
+        });
+    }
+
+
+    $('#id_processed_form-refrigerated_prior_to_processing').click(function () {
+        $('#refrigerated_placed_date_time')[this.checked ? "show" : "hide"]();
+        $('#refrigerated_removed_date_time')[this.checked ? "show" : "hide"]();
+    });
+
 });
 
 // Reload javascript if user hits back button.
